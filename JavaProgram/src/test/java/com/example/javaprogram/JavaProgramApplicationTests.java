f7da8e426f59e2e0411bc6a02056b53898f9cd51
Vladyslav Berveno <vladundancer@gmail.com>
2024-01-26 11:05:08 +0100

    added maven properties

---
+++ JavaProgram/src/test/java/com/example/javaprogram/JavaProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
